2e58f09f1d20618deef6a2790b4955dcd3af1c1a
Vasileios Rallis <vasilisrallis22@gmail.com>
2024-09-13 22:11:12 +0300

    Added license and pop up

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.getElementById('openSettings').addEventListener('click', () => {
+    // This function can open a settings page or perform some action
+    alert('Opening settings...');
+  });
+  
+  document.getElementById('refreshContent').addEventListener('click', () => {
+    // This function can refresh the content, or call other extension functions
+    alert('Refreshing content...');
+  });
+  
